9071a1091eaf76f4e14704918cab8b49174337ed
Jeremy Chon <jaedee95@gmail.com>
2019-09-17 18:55:27 -0500

    updated contact portion and highlight project icons

--- src/Contact/index.js
+++ src/Contact/index.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import LinkedIn from './Icons/linkedin.jpg'
+import GitHub from './Icons/github.png'
+import Resume from './Icons/resume.png'
+import ResumePDF from './JC_Resume.pdf'
+import { Form } from 'semantic-ui-react'
 
 class Contact extends React.Component {
 	// constructor() {
@@ -10,7 +15,67 @@ class Contact extends React.Component {
 	render() {
 		return (
 			<div className='contact' ref={this.props.toContact}>
-				<h1>Get in touch</h1>
+				<h1 className='contact-title'>Get in touch</h1>
+				<div className='contact-body'>
+					<div className='contact-info'>
+						<p className='contact-par'>​If you'd like to get in contact with me, please fill out the form with your full name, email, and reason for contacting.</p>
+						<p className='contact-par'>You may also feel free to click on the icons to check out my LinkedIn, GitHub, or resume.</p>
+						<div className='contact-icons'>
+							<a href='https://www.linkedin.com/in/jeremy-chon/' target='_blank' rel="noopener noreferrer">
+								<img 
+									className='contact-icon' 
+									src={LinkedIn} 
+									alt="linkedin icon"
+								/>
+							</a>
+							<a href="https://github.com/jeremychon" target='_blank' rel="noopener noreferrer">
+								<img 
+									className='contact-icon' 
+									src={GitHub} 
+									alt="github icon"
+								/>
+							</a>
+							<a href={ResumePDF} target='_blank' rel='noopener noreferrer'>
+								<img 
+									className='contact-icon' 
+									src={Resume} 
+									alt="resume icon"
+								/>
+							</a>
+						</div>
+					</div>
+					<Form className='contact-form'>
+						<p><span style={{color: 'rgb(199, 33, 28)'}}>*</span> indicates required field</p>
+						<div className='full-name'>
+							<Form.Input
+								required
+								label='First name' 
+								type="text" 
+								name='firstName'
+								className='user-name'
+							/>
+							<Form.Input
+								required
+								label='Last name' 
+								type="text" 
+								name='lastName'
+								className='user-name'
+							/>
+						</div>
+						<Form.Input
+							required
+							label='Email' 
+							type="text" 
+							name='email'
+						/>
+						<Form.Input
+							required
+							label='Comment' 
+							type="text" 
+							name='comment'
+						/>
+					</Form>
+				</div>
 			</div>
 		)
 	}
--- src/Work/index.js
+++ src/Work/index.js
@@ -5,26 +5,127 @@ import FlightDelay from './Photos/flight-delay.png'
 import WeightMate from './Photos/weight_mate_logo.png'
 
 class Work extends React.Component {
-	// constructor() {
-	// 	super()
-	// }
+	constructor() {
+		super()
+
+		this.state = {
+			tamaHover: false,
+			mazeHover: false,
+			flightHover: false,
+			weightHover: false
+		}
+	}
+
+	handleHoverOn = (project, e) => {
+		if (project === 'tama') {
+			this.setState({
+				tamaHover: true
+			})
+		}
+
+		if (project === 'maze') {
+			this.setState({
+				mazeHover: true
+			})
+		}
+
+		if (project === 'flight') {
+			this.setState({
+				flightHover: true
+			})
+		}
+
+		if (project === 'weight') {
+			this.setState({
+				weightHover: true
+			})
+		}	
+	}
+
+	handleHoverOff = (project, e) => {
+		if (project === 'tama') {
+			this.setState({
+				tamaHover: false
+			})
+		}
+
+		if (project === 'maze') {
+			this.setState({
+				mazeHover: false
+			})
+		}
+
+		if (project === 'flight') {
+			this.setState({
+				flightHover: false
+			})
+		}
+
+		if (project === 'weight') {
+			this.setState({
+				weightHover: false
+			})
+		}	
+	}
 
 	render() {
 		return (
 			<div className='work' ref={this.props.toWork}>
 				<h1>Work</h1>
 				<div className='work-all-projects'>
-					<div className='work-project' style={{backgroundColor: 'white'}}>
+
+					<div 
+						className='work-project' 
+						style={{backgroundColor: 'white'}}
+						onMouseOver={this.handleHoverOn.bind(null, 'tama')}
+						onMouseLeave={this.handleHoverOff.bind(null, 'tama')}
+					>
 						<img className='work-image-tama' src={Tamagotchi} alt='game character'/>
+						{this.state.tamaHover ? 
+							<div className='light-image'>
+								<h2>Tamagotchi</h2>
+							</div> 
+						: null}
 					</div>
-					<div className='work-project'>
+
+					<div 
+						className='work-project'
+						onMouseOver={this.handleHoverOn.bind(null, 'maze')}
+						onMouseLeave={this.handleHoverOff.bind(null, 'maze')}
+					>
 						<img className='work-image' src={MazeGame} alt="ship and bricks"/>
+						{this.state.mazeHover ? 
+							<div className='dark-image'>
+								<h2>Retro Maze Game</h2>
+							</div> 
+						: null}
 					</div>
-					<div className='work-project' style={{backgroundColor: 'rgb(79, 95, 118)'}}>
+
+					<div 
+						className='work-project' 
+						style={{backgroundColor: 'rgb(79, 95, 118)'}}
+						onMouseOver={this.handleHoverOn.bind(null, 'flight')}
+						onMouseLeave={this.handleHoverOff.bind(null, 'flight')}
+					>
 						<img className='work-image-flight' src={FlightDelay} alt="flight delay icon"/>
+						{this.state.flightHover ? 
+							<div className='dark-image'>
+								<h2>Flight Delay</h2>
+							</div> 
+						: null}
 					</div>
-					<div className='work-project'>
+
+					<div 
+						className='work-project'
+						onMouseOver={this.handleHoverOn.bind(null, 'weight')}
+						onMouseLeave={this.handleHoverOff.bind(null, 'weight')}
+					>
 						<img className='work-image' src={WeightMate} alt="weight mate logo"/>
+						{this.state.weightHover ? 
+							<div className='dark-image'>
+								<h2>Weight Mate</h2>
+							</div> 
+						: null}
 					</div>
 				</div>
 			</div>

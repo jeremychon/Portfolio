2b6d6484e391137b2fc441422e44fe736c645030
Jeremy Chon <jaedee95@gmail.com>
2019-10-01 18:24:58 -0500

    Updates

---
+++ precache-manifest.b02eb12f7597214b72e1bf4dae8a46ed.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f1a3030d34143cc0153ac972d1b19ee",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "95fa293e7e37244b647d",
+    "url": "/portfolio/static/css/2.1f0771ea.chunk.css"
+  },
+  {
+    "revision": "e0580d8d07afc880d742",
+    "url": "/portfolio/static/css/main.46794a9e.chunk.css"
+  },
+  {
+    "revision": "95fa293e7e37244b647d",
+    "url": "/portfolio/static/js/2.944cfe90.chunk.js"
+  },
+  {
+    "revision": "e0580d8d07afc880d742",
+    "url": "/portfolio/static/js/main.157a0f41.chunk.js"
+  },
+  {
+    "revision": "255cf37ac81f61801b31",
+    "url": "/portfolio/static/js/runtime~main.36963274.js"
+  },
+  {
+    "revision": "bd746b4524f733b9481a997b0c0d3181",
+    "url": "/portfolio/static/media/JC_Resume.bd746b45.pdf"
+  },
+  {
+    "revision": "76a2abf61fb6e0daa578127e9160914b",
+    "url": "/portfolio/static/media/Maze-Game.76a2abf6.png"
+  },
+  {
+    "revision": "5adeb67896e914beceaf4308823fa414",
+    "url": "/portfolio/static/media/Tamagotchi.5adeb678.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/portfolio/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/portfolio/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/portfolio/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/portfolio/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/portfolio/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "1aabe243a521831d5ac7302fa5e50313",
+    "url": "/portfolio/static/media/css.1aabe243.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/portfolio/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "192c541fb525789eb998838276f1ca51",
+    "url": "/portfolio/static/media/flask.192c541f.png"
+  },
+  {
+    "revision": "dd3242d55fc440840bed3918033bc182",
+    "url": "/portfolio/static/media/flight-delay.dd3242d5.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/portfolio/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/portfolio/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/portfolio/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/portfolio/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/portfolio/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "9999c1a8c347b81c5cb5df967664c4c3",
+    "url": "/portfolio/static/media/jquery.9999c1a8.jpg"
+  },
+  {
+    "revision": "e73667c4a2c3040b3057be9b7865a919",
+    "url": "/portfolio/static/media/js.e73667c4.png"
+  },
+  {
+    "revision": "5a73c91c87e54b16626ae7a6704c59d2",
+    "url": "/portfolio/static/media/mongodb.5a73c91c.png"
+  },
+  {
+    "revision": "f7337d339216d05c1551688efb13a830",
+    "url": "/portfolio/static/media/nodejs.f7337d33.jpg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/portfolio/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/portfolio/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/portfolio/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/portfolio/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/portfolio/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "ea92d6580dda0fbcf61a8ccb16e0d02a",
+    "url": "/portfolio/static/media/postgresql.ea92d658.png"
+  },
+  {
+    "revision": "febbdf76bbe46fd09797428138bfef92",
+    "url": "/portfolio/static/media/profile.febbdf76.jpg"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/portfolio/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "7a76cff597d8b80b3f1dc36a28bdd39d",
+    "url": "/portfolio/static/media/sql.7a76cff5.png"
+  },
+  {
+    "revision": "fcf2d6441a7e25cb724aefc37098e797",
+    "url": "/portfolio/static/media/weight_mate_logo.fcf2d644.png"
+  }
+]);
